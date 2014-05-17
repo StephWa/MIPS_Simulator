dc7e2f0e8426a0a7ddf68c14fce170d7455dfacb
StephWa <swallick@pdx.edu>
2014-05-17 12:31:37 -0700

    Added Prachi's op code definitions

--- MIPS_Simulator/src/Simulator.java
+++ MIPS_Simulator/src/Simulator.java
@@ -10,7 +10,7 @@ public class Simulator {
 	private BufferedReader reader;
 	private Registers Regs;
 	private Memory mem;
-	int rs,rt,rd;
+	int rs,rt,rd,im;
 	String type,inx;
 	public Simulator() throws IOException{
 		Regs = new Registers();
@@ -56,94 +56,153 @@ public class Simulator {
 		String opcode = binary_inx.subSequence(0, 6).toString();
 		switch(opcode) {
 		
+		// Add rd rs rt
 		case "000000":
 			System.out.println(opcode);
 			type = "R";
 			decode(binary_inx, type);
 			Regs.setValue(rd, Regs.R[rs]+Regs.R[rt]);
+			System.out.println(Regs.R[rd]);
 			break;
 			
+		//Addi rt rs im	
 		case "000001":
+			System.out.println(opcode);
 			type="I";
 			decode(binary_inx, type);
+			Regs.setValue(rt, Regs.R[rs]+im);
+			System.out.println(Regs.R[rt]);
 			break;
-			
+		
+		//Sub rd rs rt	
 		case "000010":
+			System.out.println(opcode);
 			type="R";
 			decode(binary_inx,type);
 			Regs.setValue(rd, Regs.R[rs]-Regs.R[rt]);
 			System.out.println(Regs.R[rd]);
 			break;
 			
+		//Subi rt rs imm		
 		case "000011":
 			System.out.println(opcode);
+			type="I";
+			decode(binary_inx, type);
+			Regs.setValue(rt, Regs.R[rs]-im);
+			System.out.println(Regs.R[rt]);
 			break;
 			
+		//Mul rd rs rt	
 		case "000100":
+			System.out.println(opcode);
 			type="R";
-			Regs.setValue(rd, Regs.R[rs]*Regs.R[rt]);
+			decode(binary_inx, type);
+			Regs.setValue(rd, Regs.R[rs] * Regs.R[rt]);
 			System.out.println(Regs.R[rd]);
 			break;
-			
+		
+		//Muli rt rs imm
 		case "000101":
 			System.out.println(opcode);
+			type="I";
+			decode(binary_inx, type);
+			Regs.setValue(rt, Regs.R[rs] * im);
+			System.out.println(Regs.R[rt]);
 			break;
-			
+		
+		//AND rd rs rt	
 		case "000110":
+			System.out.println(opcode);
 			type="R";
+			decode(binary_inx, type);
 			Regs.setValue(rd, Regs.R[rs] & Regs.R[rt]);
-			System.out.println("00000000");
+			System.out.println(Regs.R[rd]);
 			break;
-			
+		
+		//ANDi rt rs imm	
 		case "000111":
 			System.out.println(opcode);
+			type="I";
+			decode(binary_inx, type);
+			Regs.setValue(rt, Regs.R[rs] & im);
+			System.out.println(Regs.R[rt]);
 			break;
 			
+		//OR rd rs rt		
 		case "001000":
+			System.out.println(opcode);
 			type="R";
+			decode(binary_inx, type);
 			Regs.setValue(rd, Regs.R[rs] | Regs.R[rt]);
-			System.out.println("00000000");
+			System.out.println(Regs.R[rd]);
 			break;
-			
+		
+		//ORi rt rs imm		
 		case "001001":
 			System.out.println(opcode);
+			type="I";
+			decode(binary_inx, type);
+			Regs.setValue(rt, Regs.R[rs] | im);
+			System.out.println(Regs.R[rt]);
 			break;
-			
+		
+		//XOR rd rs rt	
 		case "001010":
+			System.out.println(opcode);
 			type="R";
+			decode(binary_inx, type);
 			Regs.setValue(rd, Regs.R[rs] ^ Regs.R[rt]);
-			System.out.println("00000000");
+			System.out.println(Regs.R[rd]);
 			break;
-			
+		
+		//XORi rt rs imm		
 		case "001011":
 			System.out.println(opcode);
+			type="I";
+			decode(binary_inx, type);
+			Regs.setValue(rt, Regs.R[rs] ^ im);
+			System.out.println(Regs.R[rt]);
 			break;
 			
+		//LDW Rt Rs Imm	
 		case "001100":
-			System.out.println("00000000");
+			System.out.println(opcode);
+			type="I";
+			decode(binary_inx, type);	
+			Regs.setValue(rt, Memory.getValue(Regs.R[rs] + im));
+			System.out.println(Regs.R[rt]);
 			break;
 			
+		//STW Rt Rs Imm		
 		case "001101":
 			System.out.println(opcode);
+			type="I";
+			decode(binary_inx, type);	
+			Mem.setValue(Regs.R[rs] + im, Regs.R[rt]);
+			System.out.println( Memory.getValue(Regs.R[rs] + im) );
 			break;
+		
+		//BEQ Rs Rt x	
 		case "001110":
 			System.out.println("00000000");
 			break;
 			
+		//Bz Rs x	
 		case "001111":
 			System.out.println(opcode);
 			break;
-			
+		
+		//JR Rs	
 		case "010000":
 			System.out.println(opcode);
 			break;
-			
+		
+		//HALT	
 		case "010001":
 			System.out.println("00000000");
 			break;
 			
-		case "010010":
-			System.out.println(opcode);
+
 			
 		default:
 			break;
@@ -168,6 +227,14 @@ public class Simulator {
 			break;
 		
 		case "I":
+		String rs_binary = inx.subSequence(6, 11).toString();
+		rs = Integer.parseInt(new BigInteger(rs_binary,2).toString(10));
+		String rt_binary = inx.subSequence(11,16).toString();
+		rt = Integer.parseInt(new BigInteger(rt_binary,2).toString(10));
+		//Immediate field
+		String im_binary = inx.subSequence(16,31).toString();
+		im = Integer.parseInt(new BigInteger(rd_binary,2).toString(10));
+		System.out.println(rs+" "+rt+ " "+ im);
 		
 			break;
 		
